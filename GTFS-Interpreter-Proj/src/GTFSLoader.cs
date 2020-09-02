e2168a84542d226506ec95f2a616e02b29cc33d3
ShadowFoxNixill <github@nix.nixill.net>
2020-09-01 20:02:54 -0400

    It'd be cool to have some meaningful progress.

--- GTFS-Interpreter-Proj/src/GTFSLoader.cs
+++ GTFS-Interpreter-Proj/src/GTFSLoader.cs
@@ -1,4 +1,7 @@
 using System.IO;
+using System.IO.Compression;
+using Microsoft.Data.Sqlite;
+
 namespace Nixill.GTFS {
   public class GTFSLoader {
 
@@ -10,14 +13,14 @@ namespace Nixill.GTFS {
 
       // If the path *is* a database, just load it as a database.
       if (path.EndsWith(".db")) {
-        return LoadDB(path);
+        return new GTFSObject(path);
       }
 
       // Otherwise check if the database exists.
       if (File.Exists(path + ".db")) {
         // If so, is it newer?
         if (File.GetLastWriteTimeUtc(path + ".db") > File.GetLastWriteTimeUtc(path)) {
-          return LoadDB(path + ".db");
+          return new GTFSObject(path + ".db");
         }
 
         // If it's not newer, we'll have to create the database ourself.
@@ -25,7 +28,20 @@ namespace Nixill.GTFS {
         File.Delete(path + ".db");
       }
 
-      // We'll make a new GTFS object first.
+      // So now we have to populate the database pretty much from scratch.
+      // Fortunately, we've got a template database to start with.
+      File.Copy(@"res\gtfs.db", path + ".db");
+
+      string connStr = new SqliteConnectionStringBuilder("") {
+        DataSource = path + ".db",
+        ForeignKeys = true
+      }.ToString();
+
+      using SqliteConnection conn = new SqliteConnection(connStr);
+
+      // Open the zip file
+      using ZipArchive file = ZipFile.OpenRead(path);
+
 
     }
   }
