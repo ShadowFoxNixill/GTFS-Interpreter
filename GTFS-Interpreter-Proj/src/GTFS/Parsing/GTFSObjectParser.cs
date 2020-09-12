b5da5bc3c7d073faf276f5583ec980447cb45649
ShadowFoxNixill <github@nix.nixill.net>
2020-09-12 13:41:46 -0400

    Object parser done!

--- GTFS-Interpreter-Proj/src/GTFS/Parsing/GTFSObjectParser.cs
+++ GTFS-Interpreter-Proj/src/GTFS/Parsing/GTFSObjectParser.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Nixill.Utils;
+using NodaTime;
 
 namespace Nixill.GTFS.Parsing {
   internal static class GTFSObjectParser {
@@ -11,12 +13,7 @@ namespace Nixill.GTFS.Parsing {
     private static Regex RgxDate = new Regex(@"^(\d{4})([-/\. ]?)(\d\d)([-/\. ]?)(\d\d)$");
     private static Regex RgxEmail = new Regex(@"^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b$", RegexOptions.IgnoreCase);
     private static Regex RgxLanguage = new Regex(@"^[A-Z0-9]{1,8}(-[A-Z0-9]{1,8})*", RegexOptions.IgnoreCase);
-    private static Regex RgxFloat = new Regex(@"^-?(0|[1-9]\d*)(\.\d+)?$");
-    private static Regex RgxNonNegFloat = new Regex(@"^(0|[1-9]\d*)(\.\d+)?$");
-    private static Regex RgxInt = new Regex(@"^-?(0|[1-9]\d*)$");
-    private static Regex RgxNonNegInt = new Regex(@"^(0|[1-9]\d*)$");
-    private static Regex RgxPosInt = new Regex(@"^([1-9]\d*)$");
-    private static Regex RgxTime = new Regex(@"^-?(\d\d?):(\d\d):(\d\d)$");
+    private static Regex RgxTime = new Regex(@"^(\d+):(\d\d):(\d\d)$");
 
     internal static object GetObject(GTFSDataType type, string value, ref List<string> warns) {
       // Let's get a match object and an output object ready
@@ -24,6 +21,11 @@ namespace Nixill.GTFS.Parsing {
       string ret = null;
       if (warns == null) {
         warns = new List<string>();
+      }
+
+      // Blank values should just be null.
+      if (value == "") {
+        return null;
       }
 
       // Since we're working with SQLite, all non-numbers will be returned as text.
@@ -138,14 +140,197 @@ namespace Nixill.GTFS.Parsing {
       }
 
       else if (type == GTFSDataType.Enum || type == GTFSDataType.NonNegativeInteger) {
-        if (RgxPosInt.IsMatch(value)) {
-          return int.Parse(value);
+        if (int.TryParse(value, out int val)) {
+          if (val >= 0) {
+            return val;
+          }
+          else {
+            warns.Add(value + " is not a valid non-negative integer (is negative).");
+            return null;
+          }
         }
         else {
-          warns.Add(value + " is not a valid non-negative integer.");
+          warns.Add(value + " is not a valid non-negative integer (is not a number).");
           return null;
         }
       }
+
+      else if (type == GTFSDataType.Float) {
+        if (double.TryParse(value, out double val)) {
+          return val;
+        }
+        else {
+          warns.Add(value + " is not a valid floating-point number.");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.ID) {
+        return value;
+      }
+
+      else if (type == GTFSDataType.Integer) {
+        if (int.TryParse(value, out int val)) {
+          return val;
+        }
+        else {
+          warns.Add(value + " is not a valid integer.");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.Latitude) {
+        if (float.TryParse(value, out float lat)) {
+          if (lat < -90 || lat > 90) {
+            warns.Add(value + " is not a valid latitude (out of range).");
+            return null;
+          }
+          else {
+            return lat;
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid latitude (not a number).");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.Longitude) {
+        if (float.TryParse(value, out float lon)) {
+          if (lon < -180 || lon > 180) {
+            lon %= 360;
+            if (lon > 180) {
+              lon -= 360;
+            }
+            warns.Add("Longitude was corrected to " + lon + " (was " + value + ")");
+          }
+          return lon;
+        }
+        else {
+          warns.Add(value + " is not a valid longitude (not a number).");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.NonNegativeFloat) {
+        if (double.TryParse(value, out double val)) {
+          if (val >= 0) {
+            return double.Parse(value);
+          }
+          else {
+            warns.Add(value + " is not a valid non-negative floating-point number (is negative).");
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid non-negative floating-point number (is not a number).");
+          return null;
+        }
+      }
+
+      // GTFSDataType.NonNegativeInteger: See GTFSDataType.Enum.
+
+      else if (type == GTFSDataType.Phone) {
+        // I do *not* feel like doing a single ounce of validation here.
+        // If someone wishes to change this, a pull request is welcomed.
+        return value;
+      }
+
+      else if (type == GTFSDataType.PositiveFloat) {
+        if (float.TryParse(value, out float val)) {
+          if (val > 0) {
+            return val;
+          }
+          else if (val == 0) {
+            warns.Add(value + " is not a valid positive floating-point number (is zero).");
+            return null;
+          }
+          else {
+            warns.Add(value + " is not a valid positive floating-point number (is negative).");
+            return null;
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid positive floating-point number (is not a number).");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.PositiveInteger) {
+        if (int.TryParse(value, out int val)) {
+          if (val > 0) {
+            return val;
+          }
+          else if (val == 0) {
+            warns.Add(value + " is not a valid positive integer (is zero).");
+            return null;
+          }
+          else {
+            warns.Add(value + " is not a valid positive integer (is negative).");
+            return null;
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid positive integer (is not a number).");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.Text) {
+        return value;
+      }
+
+      else if (type == GTFSDataType.Time) {
+        if (RgxTime.TryMatch(value, out match)) {
+          if (match.Groups[1].Length == 1) {
+            // I'm not going to add warnings here
+            // Because that would completely flood the log
+            return "0" + value;
+          }
+          else {
+            return value;
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid time.");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.Timezone) {
+        // Note that this is case sensitive.
+        // I don't want it to be, but I also don't feel like doing a
+        // binary search on all possible capitalizations until I find one
+        // that matches.
+        if (DateTimeZoneProviders.Tzdb.GetZoneOrNull(value) != null) {
+          return value;
+        }
+        else {
+          warns.Add(value + " is not a valid timezone.");
+          return null;
+        }
+      }
+
+      else if (type == GTFSDataType.Url) {
+        Uri uriTest;
+        if (Uri.TryCreate(value, UriKind.Absolute, out uriTest)) {
+          if (uriTest.Scheme == Uri.UriSchemeHttp) {
+            warns.Add(value + " is an http (unsecured) URL.");
+            return value;
+          }
+          if (uriTest.Scheme == Uri.UriSchemeHttps) {
+            return value;
+          }
+          else {
+            warns.Add(value + " is not a valid URL.");
+            return null;
+          }
+        }
+        else {
+          warns.Add(value + " is not a valid URI.");
+        }
+      }
+
+      return null;
     }
   }
 }
