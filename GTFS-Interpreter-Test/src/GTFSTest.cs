1a336b34fd80bb01419e6ccd7b1ec53900cc81ab
ShadowFoxNixill <github@nix.nixill.net>
2021-01-28 16:41:24 -0500

    yay for forgetting to commit things

--- GTFS-Interpreter-Test/src/GTFSTest.cs
+++ GTFS-Interpreter-Test/src/GTFSTest.cs
@@ -1,15 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Nixill.GTFS.Parsing;
 
 namespace Nixill.Testing {
   public class GTFSReaderTest {
     static void Main(string[] args) {
       Console.WriteLine("Loading file...");
-      GTFSFile file = GTFSLoader.Load("rest-2020-09-17.gtfs");
+      GTFSFile file = GTFSLoader.Load("ddot.gtfs");
       Console.WriteLine("File loaded.");
 
-      object count = file.GetResult("SELECT COUNT(*) FROM agency;");
-      Console.WriteLine("There are " + count + " agency(ies).");
+      object count = file.GetResult("SELECT COUNT(*) FROM trips;");
+      Console.WriteLine("There are " + count + " trip(s).");
+
+      Dictionary<string, object> tripsPerTime = file.GetResultDict("SELECT service_id, COUNT(trip_id) FROM trips GROUP BY service_id;");
+      foreach (string key in tripsPerTime.Keys) {
+        Console.WriteLine(key + ": " + tripsPerTime[key]);
+      }
 
       file.Dispose();
     }
